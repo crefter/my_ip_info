d8ead6fc164ee476e01867d743c41342d20c8005
crefter <crefter1@yandex.ru>
2023-08-27 00:16:18 +0700

    Delete saveRoute() from route repository

--- lib/features/map/src/domain/repository/route_repository.dart
+++ lib/features/map/src/domain/repository/route_repository.dart
@@ -2,5 +2,4 @@ import 'package:my_ip_info/features/map/src/domain/entity/route.dart';
 
 abstract interface class RouteRepository {
   Future<Route> getRoute();
-  Future<void> saveRoute(Route route);
 }
